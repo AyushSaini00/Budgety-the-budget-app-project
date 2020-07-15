843d8e27cc35caef7b18052b857f4b964476b3f7
Ayush Saini <ayushsainiiitb00@gmail.com>
2020-07-15 22:01:36 +0530

    reading input data

--- app.js
+++ app.js
@@ -9,7 +9,25 @@ var budgetController = ( function() {
 // UI CONTROLLER
 var UIController = ( function() {
     
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
     
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,   // inc and exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value 
+            };  
+        },
+        getDOMstrings: function() {
+        return DOMstrings;
+    }
+    };
     
 })();
 
@@ -18,9 +36,11 @@ var UIController = ( function() {
 // GLOBAL APP CONTROLLER
 var controller = ( function(budgetCtrl, UICtrl) {
    
+    var DOM = UICtrl.getDOMstrings();
     var ctrlAddItem = function() {
         // 1. Get the field input data
-        
+        var input = UICtrl.getInput();
+        console.log(input);
         //2. Add the item to the budget controller
         
         //3. Add the item to the UI
@@ -30,7 +50,7 @@ var controller = ( function(budgetCtrl, UICtrl) {
         //5. Display the budget on the UI
     }
     
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
     
     document.addEventListener('keypress', function() {
        if (event.keyCode === 13 || event.which === 13) {
