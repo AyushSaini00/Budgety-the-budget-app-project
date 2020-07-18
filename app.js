1842b80d207b4133a97263f05d3980c8c6b7955d
Ayush Saini <ayushsainiiitb00@gmail.com>
2020-07-18 21:24:26 +0530

    current date display

--- app.js
+++ app.js
@@ -160,7 +160,8 @@ var UIController = ( function() {
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercLabel: '.item__percentage'
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
     };
     
     var formatNumber =  function(num, type) {
@@ -275,6 +276,17 @@ var UIController = ( function() {
             });
         },
         
+        displayMonth: function() {
+            var now, months, month, year;
+            now = new Date();
+            
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            month = now.getMonth();  // Zero based i.e, January is 0
+            
+            year = now.getFullYear();
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+        },
+        
         getDOMstrings: function() {
             return DOMstrings;  // exposing the DOMstrings object into the public
     }
@@ -381,6 +393,7 @@ var controller = ( function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             console.log('App has started');
+            UICtrl.displayMonth();
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
